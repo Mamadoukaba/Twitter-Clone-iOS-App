ae989fba3b0892242ff0924fe4605b4ba3e65a0a
Mamadou Kaba <mamkab23@gmail.com>
2016-06-22 23:33:28 -0400

    First Commit

---
+++ Tags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tags
+//
+//  Created by Mamadou Kaba on 6/21/16.
+//  Copyright © 2016 Mamadou Kaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
